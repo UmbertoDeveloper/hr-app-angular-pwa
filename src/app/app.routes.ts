7f53c320a80732b5428157fb38030942cbe87dcb
UmbertoPC <umberto.menichetti@icloud.com>
2024-10-07 22:21:18 +0200

    first step on search_time_report

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'search-time-report',
+        pathMatch: 'full',
+    },
+    {
+        path: 'search-time-report',
+        loadComponent() {
+            return import('./search-time-report/search-time-report.component').then(m => m.SearchTimeReportComponent);
+        },
+    }
+];
